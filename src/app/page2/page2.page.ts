62cafa725ff0586c413dbc02b4536666f461df12
Damian Tarnawsky <damian@ionic.io>
2021-08-02 07:08:58 -0700

    Initial Commit

---
+++ src/app/page2/page2.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-page2',
+  templateUrl: './page2.page.html',
+  styleUrls: ['./page2.page.scss'],
+})
+export class Page2Page implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  page3() {
+    this.router.navigateByUrl('page3');
+  }
+
+}
