c478aa15c49e9a34369b8dc3995498fba4e28b5d
Damian Tarnawsky <damian@ionic.io>
2021-08-02 07:52:23 -0700

    Add capacitor ios and android projects

---
+++ android/app/src/main/java/com/cs/swipe/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cs.swipe;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cs.swipe',
+  appName: 'cs-swipe',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
